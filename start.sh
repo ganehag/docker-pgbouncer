4a0e6cc332e612e82882403e0a448c17d2151ebc
Mikael Ganehag Brorsson <mikael.brorsson@gmail.com>
2016-09-27 09:07:17 +0200

    Changed to 1 sec

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ rm /var/run/pgbouncer/*.pid
 
 until [ -f /etc/pgbouncer/pgbouncer.ini ]; do
      echo "waiting for config file..."
-     sleep 5
+     sleep 1
 done
 
 /usr/bin/pgbouncer /etc/pgbouncer/pgbouncer.ini
